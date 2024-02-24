ec222efba4a7c2d2873affd59887eae6463164ef
Major-Scott <99700517+Major-Scott@users.noreply.github.com>
2024-02-24 00:35:54 -0800

    Add project files.

---
+++ AutoSwitch/ASPatch/PlayerControllerBPatch.cs
@@ -0,0 +1,37 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSwitch.ASPatch
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("DiscardHeldObject")]
+        [HarmonyPostfix]
+        private static void autoSwitchPatch(PlayerControllerB __instance, ref GrabbableObject[] ___ItemSlots, ref bool ___isHoldingObject)
+        {
+            MethodInfo method = typeof(PlayerControllerB).GetMethod("NextItemSlot", BindingFlags.Instance | BindingFlags.NonPublic);
+            MethodInfo method2 = typeof(PlayerControllerB).GetMethod("SwitchToItemSlot", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (!(method != null) || !(method2 != null))
+            {
+                return;
+            }
+            int currentItemSlot = __instance.currentItemSlot;
+            for (int num = (int)method.Invoke(__instance, new object[1] { true }); num != currentItemSlot; num = (num + 1) % ___ItemSlots.Length)
+            {
+                if ((Object)(object)___ItemSlots[num] != null)
+                {
+                    method2.Invoke(__instance, new object[2] { num, null });
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ AutoSwitch/AutoSwitchMain.cs
@@ -0,0 +1,40 @@
+﻿using AutoSwitch.ASPatch;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSwitch
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class AutoSwitchMain : BaseUnityPlugin
+    {
+        private const string modGUID = "Slam.AutoSwitch";
+        private const string modName = "Auto Switch";
+        private const string modVersion = "0.1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static AutoSwitchMain instance;
+
+        internal ManualLogSource mls;
+
+
+        void Awake()
+        {
+            if (instance == null) { instance = this; }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource("Slam.AutoSwitch");
+            mls.LogInfo("Auto Switch Mod has loaded!");
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+
+            harmony.PatchAll(typeof(AutoSwitchMain));
+        }
+
+
+    }
+}
